59c218f6eb75744362d95b1fd12850784e61bc23
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-06-27 15:52:01 -0300

    Projeto inicial

---
+++ src/main/java/br/ufjf/dcc193/trbop/TrabalhoOpcionalApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trbop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoOpcionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoOpcionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/dcc193/trbop/TrabalhoOpcionalApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc193.trbop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrabalhoOpcionalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
